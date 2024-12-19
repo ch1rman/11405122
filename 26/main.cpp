248e9ec7386bb7b8b9c82de89216ecb1f3052c74
ISMETVAKILOV <ismetvakilov6@gmail.com>
2024-12-19 21:16:36 +0300

    main.cpp

--- 26/main.cpp
+++ 26/main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; // Дек для хранения индексов элементов окна
+        deque<int> dq; 
         vector<int> result;
 
         for (int i = 0; i < nums.size(); ++i) {
-            // Удаляем элементы, которые вышли за пределы окна
+            
             if (!dq.empty() && dq.front() == i - k) {
                 dq.pop_front();
             }
 
-            // Удаляем элементы, которые меньше текущего
+           
             while (!dq.empty() && nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
 
-            // Добавляем текущий элемент в дек
+            
             dq.push_back(i);
 
-            // Добавляем максимум окна в результат (начиная с момента, когда окно заполнено)
+            
             if (i >= k - 1) {
                 result.push_back(nums[dq.front()]);
             }
@@ -36,14 +36,14 @@ public:
 int main() {
     Solution solution;
 
-    // Ввод данных
+   
     vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
     int k = 3;
 
-    // Решение задачи
+ 
     vector<int> result = solution.maxSlidingWindow(nums, k);
 
-    // Вывод результата
+    
     cout << "Max sliding window: ";
     for (int num : result) {
         cout << num << " ";
