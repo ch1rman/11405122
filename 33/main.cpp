20b4202e040e73f8bb379dc44f6949c716a654c4
ch1rman <liskusilde@gmail.com>
2024-12-23 17:14:17 +0300

    33.cpp

--- 33/main.cpp
+++ 33/main.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
+ int maxProfit(vector<int>& prices) {
+       if(!prices.size()) return 0;
+       int maximum = 0;
+       for(int i=1; i<prices.size(); i++)
+            maximum = (prices[i]>prices[i-1]) ? (maximum+prices[i]-prices[i-1]) : maximum;
+       return maximum;
     }
 };
-
